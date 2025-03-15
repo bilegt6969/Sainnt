27901b16e1482ba69684393045d2b18cc67a921a
“Bilegt6969” <bilegtxyz@gmail.com>
2025-03-15 19:19:14 +0800

    attemp9

---
+++ src/app/(frontend)/brands/[brand]/BrandPageWrapper.tsx
@@ -0,0 +1,16 @@
+'use client'
+
+import { Suspense } from 'react'
+import dynamic from 'next/dynamic'
+
+// Dynamically import the client component with no SSR
+// This is now in a client component, so ssr: false is allowed
+const BrandPageClient = dynamic(() => import('./client'), { ssr: false })
+
+export default function BrandPageWrapper({ brand }: { brand: string }) {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <BrandPageClient brand={brand} />
+    </Suspense>
+  )
+}
